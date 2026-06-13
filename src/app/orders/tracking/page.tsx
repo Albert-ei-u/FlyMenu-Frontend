632a72eb5e03de310e1a49cade2ffc83a9339b72
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-06-13 23:52:46 +0200

    standardized Lazy Loading

--- src/app/orders/tracking/page.tsx
+++ src/app/orders/tracking/page.tsx
@@ -24,7 +24,6 @@ export default function OrderTrackingPage() {
   const [rating, setRating] = useState(4);
   const [selectedTags, setSelectedTags] = useState<string[]>([
     "Great Presentation",
-    
   ]);
 
   useEffect(() => {
