d70eb61e836d981544564e1abdfa14e1c70e7149
albert-ei-u <uwumuremyialbert5@gmail.com>
2026-06-20 21:05:51 +0200

    sampling

--- src/app/orders/tracking/page.tsx
+++ src/app/orders/tracking/page.tsx
@@ -22,7 +22,7 @@ export default function OrderTrackingPage() {
   const [rating, setRating] = useState(4);
 
   const [selectedTags, setSelectedTags] = useState<string[]>([
-    "Great Presentation",
+    "Great Presentation",\
   ]);
 
   useEffect(() => {
